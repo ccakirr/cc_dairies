4446ee791c0274d5a1df00493263d227db85bdcb
Caner Çakır <55649866+Canerss01@users.noreply.github.com>
2025-08-25 22:11:46 +0300

    login required page added login form added  login page added homepage edited for logged in users add article feature wil be added.

--- blog.py
+++ blog.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template, flash, redirect,url_for,session,loggin
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
+from functools import wraps
+
+#kullanıcı giriş decorator' ı
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "logged_in" in session:
+            return f(*args, **kwargs)
+        else:
+            flash("Bu sayfayı görüntülemek için giriş yapınız!","danger")
+            return (redirect(url_for("login")))
+    return decorated_function
 
 class RegisterForm(Form):
     username = StringField("Kullanıcı Adı", validators=[validators.Length(min=4, max=8)])
@@ -11,6 +23,10 @@ class RegisterForm(Form):
         validators.EqualTo(fieldname="confirm", message="Parolalar uyuşmuyor")
     ])
     confirm = PasswordField("Parola doğrula")
+
+class LoginForm(Form):
+    username = StringField("Kullanıcı Adı")
+    password = PasswordField("Şifre")
 
 app = Flask(__name__)
 app.config["MYSQL_HOST"] = "localhost"
@@ -41,9 +57,40 @@ def register():
         mysql.connection.commit()
         cursor.close()
         flash("Başarıyla Kayıt Oldunuz.", "success")
-        return (redirect(url_for("index")))
+        return (redirect(url_for("login")))
     else:
         return render_template("register.html",form = form)
 
+@app.route("/login", methods = ["POST","GET"])
+def login():
+    form = LoginForm(request.form)
+    if(request.method == "POST" and form.validate()):
+        username = form.username.data
+        password = form.password.data
+        cursor = mysql.connection.cursor()
+        data = cursor.execute("Select * from users where username = %s", (username,))
+        if (data > 0):
+            data_by_username = cursor.fetchone()
+            if (sha256_crypt.verify(password, data_by_username["password"])):
+                flash("Başarıyla giriş yapıldı.","success")
+                session["logged_in"] = True
+                session["username"] = username
+                return(redirect(url_for("index")))
+            else:
+                flash("Hatalı parola.","danger")
+                return (redirect(url_for("login")))
+        else:
+            flash("Hatalı kullanıcı adı.","danger")
+            return redirect(url_for("login"))
+    return (render_template("login.html",form=form))
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return(render_template("dashboard.html"))
+@app.route("/logout")
+def logout():
+    session.clear()
+    flash("Başarıyla çıkış yapıldı.", "success")
+    return (redirect(url_for("index")))
 if(__name__ == "__main__"):
     app.run(debug=True)
